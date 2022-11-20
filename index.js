78865afe6e210b91e33c1bcf94f88620988d0b1b
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-11-19 19:24:34 -0500

    fixed form validation

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const fName = document.getElementsByClassName(".firstName");
-const lName = document.getElementsByClassName(".lastName");
-const email = document.getElementsByClassName(".email");
-const password = document.getElementsByClassName(".password");
-const button = document.getElementsByClassName("claim");
-const error = document.getElementsByClassName(".error");
+//inputs
+const fName = document.getElementById("firstName");
+const lName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const button = document.getElementById("claim");
+//error-icons
+const fError = document.getElementById("first-error");
+const lError = document.getElementById("last-error");
+const eError = document.getElementById("email-error");
+const pError = document.getElementById("password-error");
+//errorMSG
+const eMsg = document.getElementById('errorMsg');
 
-button.addEventListener("click", errorMsg);
+fError.classList.add('hidden');
+lError.classList.add('hidden');
+eError.classList.add('hidden');
+pError.classList.add('hidden');
+button.addEventListener('click', error);
 
-function errorMsg(){
-    if(!fName.checkValidity() || fName.value === ""){
-        error.style.visibility = "visible";
+//FUNCTIONS
+function error(e){
+    if(email.validity.typeMismatch){
+        eError.classList.remove('hidden');
+        eMsg.innerHTML = "Invalid email";
     }
-    else if(!email.checkValidity() || email.value === ""){
-        error.style.visibility = "visible";
+    else if(email.value===""){
+        eError.classList.remove('hidden');
+        e.preventDefault();
+        eMsg.innerHTML = "Enter an email";
     }
-    else if(!lName.checkValidity() || email.value === ""){
-        error.style.visibility = "visible";
-    }
-    else if(!password.checkValidity() || email.value === ""){
-        error.style.visibility = "visible";
-    }
-
 }
